b6895fd9b08039fad1afc83c46777ee473bc859c
Blake Williams <duckbill43@gmail.com>
2019-11-11 14:26:41 -0500

    cleaned up boxing functions and fixed the misalignment issue by only using two corners

--- bounding.py
+++ bounding.py
@@ -3,80 +3,43 @@ from google.cloud import vision
 import io
 
 
-def main_test(corners, image_obj):
-    # make corners comply with list of dict format
+def main_test():
+    pass
+
+
+def place_box(a_corners, image_obj, color=(0, 254, 0)):
+    # make sure corners comply with list of dict format
+    # top left and bottom right corners only
     # e.g. [{"x": 50, "y": 100},
-    #       {"x":100, "y": 100}, ... ]
+    #       {"x": 80, "y": 150}]
 
     width = int(image_obj.size[1] / 300)
 
     # Draw top line
-    for i in range(corners[0]['y'], corners[0]['y'] + width):
-        for j in range(corners[0]['x'], corners[1]['x']):
-            image_obj.putpixel((j, i), (0, 254, 0))
+    for i in range(a_corners[0]['y'], a_corners[0]['y'] + width + 1):
+        for j in range(a_corners[0]['x'], a_corners[1]['x'] + 1):
+            image_obj.putpixel((j, i), color)
 
     # Draw bottom line
-    for i in range(corners[2]['y'], corners[2]['y'] + width):
-        for j in range(corners[3]['x'], corners[2][
-                                            'x'] + width):  # google draws their corners in a circular pattern
-            image_obj.putpixel((j, i), (0, 254, 0))
+    for i in range(a_corners[1]['y'] - width, a_corners[1]['y'] + 1):
+        for j in range(a_corners[0]['x'], a_corners[1]['x'] + 1):  # google draws their corners in a circular pattern
+            image_obj.putpixel((j, i), color)
 
     # Draw left line
-    for i in range(corners[0]['y'], corners[3]['y']):
-        for j in range(corners[0]['x'], corners[0]['x'] + width):
-            image_obj.putpixel((j, i), (0, 254, 0))
+    for i in range(a_corners[0]['y'], a_corners[1]['y'] + 1):
+        for j in range(a_corners[0]['x'], a_corners[0]['x'] + width + 1):
+            image_obj.putpixel((j, i), color)
 
     # Draw right line
-    for i in range(corners[1]['y'], corners[2]['y']):
-        for j in range(corners[1]['x'], corners[1]['x'] + width):
-            image_obj.putpixel((j, i), (0, 254, 0))
+    for i in range(a_corners[0]['y'], a_corners[1]['y'] + 1):
+        for j in range(a_corners[1]['x'] - width, a_corners[1]['x'] + 1):
+            image_obj.putpixel((j, i), color)
 
     return image_obj
 
 
-def place_box(corners, image_obj, color=(0, 254, 0)):
-    # make corners comply with list of dict format
-    # e.g. [{"x": 50, "y": 100},
-    #       {"x":100, "y": 100}, ... ]
-
-    width = int(image_obj.size[1] / 300)
-
-    # Draw top line
-    for i in range(corners[0]['y'], corners[0]['y'] + width):
-        for j in range(corners[0]['x'], corners[1]['x']):
-            image_obj.putpixel((j, i), color)
-
-    # Draw bottom line
-    for i in range(corners[2]['y'], corners[2]['y'] + width):
-        for j in range(corners[3]['x'], corners[2][
-                                            'x'] + width):  # google draws their corners in a circular pattern
-            image_obj.putpixel((j, i), color)
-
-    # Draw left line
-    for i in range(corners[0]['y'], corners[3]['y']):
-        for j in range(corners[0]['x'], corners[0]['x'] + width):
-            image_obj.putpixel((j, i), color)
-
-    # Draw right line
-    for i in range(corners[1]['y'], corners[2]['y']):
-        for j in range(corners[1]['x'], corners[1]['x'] + width):
-            image_obj.putpixel((j, i), color)
-
-    return image_obj
-
-def box_document(path, box_words=True, box_letters=False):
-    """Detects document features in an image."""
-
-    client = vision.ImageAnnotatorClient.from_service_account_json('creds.json')
-
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-
-    image = vision.types.Image(content=content)
-
-    response = client.document_text_detection(image=image)
-
-    boxes = []      # list of list of dict
+def get_corners(response, box_words=True, box_letters=False, box_paragraphs=False):
+    boxes = []  # list of list of dict
 
     for page in response.full_text_annotation.pages:
         for block in page.blocks:
@@ -84,20 +47,62 @@ def box_document(path, box_words=True, box_letters=False):
                 for word in paragraph.words:
                     for symbol in word.symbols:
                         if box_letters:
-                            box = []
-                            for vertex in symbol.bounding_box.vertices:
-                                box.append({'x': vertex.x, 'y': vertex.y})
+                            box = [
+                                {
+                                    'x': symbol.bounding_box.vertices[0].x,
+                                    'y': symbol.bounding_box.vertices[0].y
+                                },
+                                {
+                                    'x': symbol.bounding_box.vertices[2].x,
+                                    'y': symbol.bounding_box.vertices[2].y
+                                },
+                            ]
                             boxes.append(box)
-                    # draw larger box after small ones
+                    # draw larger boxes after small ones
                     if box_words:
-                        box = []
-                        for vertex in word.bounding_box.vertices:
-                            box.append({'x': vertex.x,
-                                        'y': vertex.y,
-                                        'color': (254, 0, 0)})
+                        box = [
+                            {
+                                'x': word.bounding_box.vertices[0].x,
+                                'y': word.bounding_box.vertices[0].y,
+                                'color': (255, 0, 0)
+                            },
+                            {
+                                'x': word.bounding_box.vertices[2].x,
+                                'y': word.bounding_box.vertices[2].y,
+                            },
+                        ]
                         boxes.append(box)
+                # draw larger boxes after small ones
+                if box_paragraphs:
+                    box = [
+                        {
+                            'x': paragraph.bounding_box.vertices[0].x,
+                            'y': paragraph.bounding_box.vertices[0].y,
+                            'color': (0, 0, 255)
+                        },
+                        {
+                            'x': paragraph.bounding_box.vertices[2].x,
+                            'y': paragraph.bounding_box.vertices[2].y,
+                        },
+                    ]
+                    boxes.append(box)
 
-    box_image = Image.open(path)
+    return boxes
+
+
+def box_document(a_path, **kwargs):
+
+    client = vision.ImageAnnotatorClient.from_service_account_json('creds.json')
+
+    with io.open(a_path, 'rb') as image_file:
+        content = image_file.read()
+
+    o_image = vision.types.Image(content=content)
+    response = client.document_text_detection(image=o_image)
+
+    boxes = get_corners(response, **kwargs)
+    box_image = Image.open(a_path)
+
     for box in boxes:
         if 'color' in box[0].keys():
             box_image = place_box(box, box_image, box[0]['color'])
@@ -108,29 +113,4 @@ def box_document(path, box_words=True, box_letters=False):
 
 
 if __name__ == '__main__':
-    # path = 'devocion_test.jpg'
-    # path = 'devocion_test copy.jpg'
-    path = 'handwriting.png'
-
-    corners = [
-        {   # Corner 0
-            "x": 360,
-            "y": 59
-        },
-        {   # Corner 1
-            "x": 408,
-            "y": 59
-        },
-        {   # Corner 2
-            "x": 408,
-            "y": 102
-        },
-        {   # Corner 3
-            "x": 360,
-            "y": 102
-        }
-    ]
-
-    image = Image.open(path)
-    new_image = place_box(corners, image)
-    new_image.show()
+    pass
--- main_test.py
+++ main_test.py
@@ -3,32 +3,32 @@ import io
 from bounding import box_document
 
 
-def detect_document(path):
+def detect_document(image_path, **kwargs):
     """Detects document features in an image."""
 
-    client = vision.ImageAnnotatorClient.from_service_account_json('creds.json')
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-    image = vision.types.Image(content=content)
-    response = client.document_text_detection(image=image)
+    # client = vision.ImageAnnotatorClient.from_service_account_json('creds.json')
+    # with io.open(image_path, 'rb') as image_file:
+    #     content = image_file.read()
+    # image = vision.types.Image(content=content)
+    # response = client.document_text_detection(image=image)
+    #
+    # words = []
+    #
+    # for page in response.full_text_annotation.pages:
+    #     for block in page.blocks:
+    #         for paragraph in block.paragraphs:
+    #             for word in paragraph.words:
+    #                 curr_word = ''
+    #                 for symbol in word.symbols:
+    #                     curr_word += symbol.text
+    #                 words.append({'word': curr_word,
+    #                               'confidence': word.confidence})
+    #
+    # for word in words:
+    #     print(word)
 
-    words = []
-
-    for page in response.full_text_annotation.pages:
-        for block in page.blocks:
-            for paragraph in block.paragraphs:
-                for word in paragraph.words:
-                    curr_word = ''
-                    for symbol in word.symbols:
-                        curr_word += symbol.text
-                    words.append({'word': curr_word,
-                                  'confidence': word.confidence})
-
-    for word in words:
-        print(word)
-
-    # box_image = box_document(path, box_words=True)
-    # box_image.show()
+    box_image = box_document(image_path, **kwargs)
+    box_image.show()
 
 
 if __name__ == '__main__':
@@ -36,4 +36,7 @@ if __name__ == '__main__':
     # path = 'devocion_test copy.jpg'
     # path = 'handwriting.png'
 
-    detect_document(path)
+    # new_image = box_document(path)
+    # new_image.show()
+
+    detect_document(path, box_paragraphs=True, box_letters=True)
