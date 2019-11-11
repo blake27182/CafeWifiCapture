841126c161a5f193fa11e73768a8956df89d9fc4
Blake Williams <duckbill43@gmail.com>
2019-11-11 12:08:13 -0500

    Separated boxing functions from main detect function

--- bounding.py
+++ bounding.py
@@ -1,5 +1,6 @@
 from PIL import Image
-import base64
+from google.cloud import vision
+import io
 
 
 def main_test(corners, image_obj):
@@ -62,6 +63,47 @@ def place_box(corners, image_obj, color=(0, 254, 0)):
             image_obj.putpixel((j, i), color)
 
     return image_obj
+
+def box_document(path, box_words=False):
+    """Detects document features in an image."""
+
+    client = vision.ImageAnnotatorClient.from_service_account_json('creds.json')
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+
+    boxes = []      # list of list of dict
+
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    for symbol in word.symbols:
+                        box = []
+                        for vertex in symbol.bounding_box.vertices:
+                            box.append({'x': vertex.x, 'y': vertex.y})
+                        boxes.append(box)
+                    # draw larger box after small ones
+                    if box_words:
+                        box = []
+                        for vertex in word.bounding_box.vertices:
+                            box.append({'x': vertex.x,
+                                        'y': vertex.y,
+                                        'color': (254, 0, 0)})
+                        boxes.append(box)
+
+    box_image = Image.open(path)
+    for box in boxes:
+        if 'color' in box[0].keys():
+            box_image = place_box(box, box_image, box[0]['color'])
+        else:
+            box_image = place_box(box, box_image)
+
+    return box_image
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -1,53 +1,24 @@
 from google.cloud import vision
-from PIL import Image as pilImage
-import io, requests, json
-from bounding import place_box
+import io
+from bounding import box_document
 
 
 def detect_document(path):
     """Detects document features in an image."""
 
-    client = vision.ImageAnnotatorClient.from_service_account_json('creds.json')
+    # client = vision.ImageAnnotatorClient.from_service_account_json('creds.json')
+    # with io.open(path, 'rb') as image_file:
+    #     content = image_file.read()
+    # image = vision.types.Image(content=content)
+    # response = client.document_text_detection(image=image)
 
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-
-    image = vision.types.Image(content=content)
-
-    response = client.document_text_detection(image=image)
-
-    boxes = []      # list of list of dict
-
-    for page in response.full_text_annotation.pages:
-        for block in page.blocks:
-            for paragraph in block.paragraphs:
-                for word in paragraph.words:
-                    for symbol in word.symbols:
-                        box = []
-                        for vertex in symbol.bounding_box.vertices:
-                            box.append({'x': vertex.x, 'y': vertex.y})
-                        boxes.append(box)
-                    # draw larger box after small ones
-                    box = []
-                    for vertex in word.bounding_box.vertices:
-                        box.append({'x': vertex.x,
-                                    'y': vertex.y,
-                                    'color': (254, 0, 0)})
-                    boxes.append(box)
-
-    box_image = pilImage.open(path)
-    for box in boxes:
-        if 'color' in box[0].keys():
-            box_image = place_box(box, box_image, box[0]['color'])
-        else:
-            box_image = place_box(box, box_image)
-
+    box_image = box_document(path)
     box_image.show()
 
 
 if __name__ == '__main__':
-    # path = "devocion_test.jpg"
-    path = 'devocion_test copy.jpg'
+    path = "devocion_test.jpg"
+    # path = 'devocion_test copy.jpg'
     # path = 'handwriting.png'
 
     detect_document(path)
